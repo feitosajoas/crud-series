47098a3a847e29c9193319d2484b0ece57a12ace
joasfeitosadelimasilva@gmail.com <joasfeitosadelimasilva@gmail.com>
2021-10-22 00:22:40 -0300

    Desenvolvendo operações de CRUD generica

---
+++ Series/Entities/SerieRepositorio.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Series.Entities.Interface;
+
+namespace Series.Entities
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Proximo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornarPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
